e79e03609cc7ff261f6128a27d8711dc9b6776ab
Ziyad Edher <ziyad.edher@gmail.com>
2018-05-08 10:37:19 -0400

    Create boilerplate project structure

---
+++ start.py
@@ -0,0 +1,15 @@
+"""Entry point to the program."""
+
+
+from src.gui import gui
+from src.game import game
+from src.strategy import strategy
+
+
+def main() -> None:
+    """Begins the program."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
